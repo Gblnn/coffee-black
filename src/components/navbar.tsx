1695bb399fddc3c55a4342c981eae1199ae85e11
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-09 04:30:15 +0400

    update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,35 +1,57 @@
 import { Link, useLocation } from "react-router-dom";
 import CoffeeBean from "./icons/coffeebean";
 import { Coffee, MessageSquareDot, Plus, User } from "lucide-react";
-import Dropdown from "./dropdown";
+import { Drawer, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle } from "./ui/drawer";
+import { Button } from "antd";
+import { useState } from "react";
 
 
 export default function Navbar() {
 
+    const [draweropen, setDraweropen] = useState(false)
     const location = useLocation()
     const accent = "salmon"
 
-    
+    const setDrawer = () =>{
+        if(!draweropen){
+            setDraweropen(true)
+        }
+        else{
+            setDraweropen(false)
+        }
+        
+    }    
 
     return(
         <div className="nav-container">
-                <nav className="nav"> 
-                    
-                        <Link className={location.pathname=="/feed"?"active nav-item":"nav-item"} to="/feed"><CoffeeBean fill={location.pathname=="/feed"?accent:"#8a8a8a"}/></Link>
-                    
-                        <Link className={location.pathname=="/brews"?"active nav-item":"nav-item"} to="/brews"><Coffee stroke={location.pathname=="/brews"?accent:"#8a8a8a"}/></Link>
-                    
-                        <Link className={location.pathname=="/liked"?"active nav-item":"nav-item"} to="/liked"><Plus stroke={location.pathname=="/liked"?accent:"#8a8a8a"} /></Link>
-         
-                        <Link className={location.pathname=="/posts"?"active nav-item":"nav-item"} to="/posts"><MessageSquareDot stroke={location.pathname=="/posts"?accent:"#8a8a8a"}  /></Link>
-                    
-                        <Link className={location.pathname=="/saved"?"active nav-item":"nav-item"} to="/saved"><User stroke={location.pathname=="/saved"?accent:"#8a8a8a"} /></Link>
 
-                        <Dropdown title="Account" item1="View Profile" item2="Logout"/>
+            <nav className="nav"> 
                     
-                   
+                <Link className={location.pathname=="/feed"?"active nav-item":"nav-item"} to="/feed"><CoffeeBean fill={location.pathname=="/feed"?accent:"#8a8a8a"}/></Link>
                     
-                </nav>
+                <Link className={location.pathname=="/brews"?"active nav-item":"nav-item"} to="/brews"><Coffee stroke={location.pathname=="/brews"?accent:"#8a8a8a"}/></Link>
+
+
+                <Link onClick={setDrawer} className={location.pathname=="/liked"?"active nav-item":"nav-item"} to="/liked"><Plus stroke={location.pathname=="/liked"?accent:"#8a8a8a"} /></Link>
+         
+                <Link className={location.pathname=="/posts"?"active nav-item":"nav-item"} to="/posts"><MessageSquareDot stroke={location.pathname=="/posts"?accent:"#8a8a8a"}  /></Link>
+                    
+                <Link className={location.pathname=="/saved"?"active nav-item":"nav-item"} to="/saved"><User stroke={location.pathname=="/saved"?accent:"#8a8a8a"} /></Link>
+
+            </nav>
+            <Drawer open={draweropen}>
+                <DrawerContent className="drawer">
+                    <DrawerHeader>
+                        <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                        <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter className="flex">
+                        <Button>Submit</Button>
+                        <Button onClick={setDrawer}>Cancel</Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
             </div>
+            
     )
 }
