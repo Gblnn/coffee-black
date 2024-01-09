1a1e84dea4659296a569aa0aacb1c799230127a3
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-09 07:41:02 +0400

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import CoffeeBean from "./icons/coffeebean";
-import { Coffee, Inbox, Plus, User } from "lucide-react";
+import { Coffee, Inbox, PlusSquare, User } from "lucide-react";
 import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from "./ui/drawer";
 import { useState } from "react";
 
@@ -32,7 +32,7 @@ export default function Navbar() {
                 <Link className={location.pathname=="/brews"?"active nav-item":"nav-item"} to="/brews"><Coffee stroke={location.pathname=="/brews"?accent:"#6a6a6a"}/></Link>
 
 
-                <button onClick={setDrawer} className={location.pathname=="/liked"?"active nav-item":"create"} ><Plus stroke={location.pathname=="/liked"?accent:"#6a6a6a"} /></button>
+                <button onClick={setDrawer} className={location.pathname=="/liked"?"active nav-item":"create"} ><PlusSquare stroke={location.pathname=="/liked"?accent:"#6a6a6a"} /></button>
          
                 <Link className={location.pathname=="/posts"?"active nav-item":"nav-item"} to="/posts"><Inbox stroke={location.pathname=="/posts"?accent:"#6a6a6a"}  /></Link>
                     
