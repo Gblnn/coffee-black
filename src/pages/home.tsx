3271d7aea30167d7d6ad8bdfc28bb0cb59800c51
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-11 15:20:36 +0400

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,16 @@
-import '@/app/globals.css'
-import '@/styles/style.css'
-import '@/styles/navbar.css'
+import '@/app/globals.css';
 import Navbar from "@/components/navbar";
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import Feed from './feed';
+import '@/styles/navbar.css';
+import '@/styles/style.css';
+import { Route, Routes } from 'react-router-dom';
 import Brews from './brews';
+import Feed from './feed';
 import Posts from './posts';
 import Saved from './saved';
-import { useEffect } from 'react';
 
 export default function Home(){
 
-    const usenavigate = useNavigate()
-
-  useEffect(()=>{
-    usenavigate('/feed')
-
-    // if(window.name==""){
-    // }
-
-},[usenavigate])
+  
     return(
         <>
             <Navbar/>
