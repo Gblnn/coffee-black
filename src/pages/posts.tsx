0f1e7a2640a26b2a5e5b3119ac4ac7667ad1a401
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-16 10:56:42 +0400

    update

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -2,7 +2,14 @@
 export default function Posts(){
     return(
         <>
-        <h1 style={{color:"white"}}>INBOX</h1>
+        <div className="inbox" style={{width:"100svw", height:"100svh", border:"", background:"linear-gradient(black, #1a1a1a)"}}>
+            <div style={{width:"100%", height:"100%", padding:"", border:"", borderRadius:"1rem", background:"#1a1a1a"}}>
+                <div style={{width:"30ch", height:"100%", background:"#111111", borderTopLeftRadius:"1rem", borderBottomLeftRadius:"1rem"}}>
+
+                </div>
+            </div>
+
+        </div>
         </>
     )
 }
