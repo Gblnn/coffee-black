93e7c8849ebc7ef12cbe13d6afb6c53c937842d7
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-17 21:08:39 +0400

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,4 @@
-import { Button, ConfigProvider, Form, Input, Typography, message, theme } from "antd";
+import { Button, ConfigProvider, Form, Typography, message, theme } from "antd";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -73,25 +73,25 @@ export default function Login() {
     return(
         <>
         <div style={{display:"flex",justifyContent:"center",alignItems:"center",width:"100%",height:"100svh", border:""}}>
-        <div className="auth-card" style={{ height:"fit-content", background:"#1a1a1a", borderRadius:"1rem"}}>
+        <div className="auth-card" style={{ height:"fit-content", background:"var(--card-color)", borderRadius:"1rem"}}>
             <div style={{padding:"1.5rem", border:"", height:"100%"}}>
                 <div style={{display:"flex", gap:"0.25rem"}}>
-                <img style={{width:"2.5rem"}} src="/coffee-bag-color.png"/>
+                {/* <img style={{width:"2.5rem"}} src="/coffee-bag-color.png"/> */}
                 <h1 style={{fontWeight:600, fontSize:"1.75rem"}}>COFFEE</h1>
                 </div>
                 
-                <ConfigProvider theme={{algorithm: theme.darkAlgorithm, token:{colorPrimary:"salmon"} }}>
+                <ConfigProvider theme={{algorithm: theme.darkAlgorithm,token:{colorPrimary:"salmon",colorBgContainerDisabled:"rgba(100 100 100 / 10%)", colorTextDisabled:"#6a6a6a"} }}>
                     <Form style={{marginTop:"2.75rem"}}>
                         <div style={{display:"flex", flexFlow:"column", gap:"1rem"}}>
                             
-                            <Input onChange={e=>setUsername(e.target.value.toLowerCase())} style={{fontFamily:"Clash Grotesk", fontSize:"16px"}} placeholder="Username"/>
+                            <input onChange={e=>setUsername(e.target.value.toLowerCase())} style={{fontFamily:"Clash Grotesk", fontSize:"16px"}} placeholder="Username"/>
                             
-                            <Input.Password onChange={e=>setPassword(e.target.value)} style={{fontFamily:"Clash Grotesk", fontSize:"16px"}} placeholder="Password"/>
+                            <input type="password" onChange={e=>setPassword(e.target.value)} style={{fontFamily:"Clash Grotesk", fontSize:"16px"}} placeholder="Password"/>
                             
                         
                         <Button htmlType="submit" onClick={Validate} loading={loading} disabled={!postable} style={{marginTop:"1.5rem"}}>LOGIN</Button>
                         
-                        <Typography style={{marginTop:"0.5rem",fontFamily:"Clash Grotesk", textAlign:"center", color:"rgba(255 255 255/ 50%)"}}>Dont have an account? <Link to="/signup" style={{color:"white"}}>Sign-in</Link></Typography>
+                        <Typography style={{marginTop:"0.5rem",fontFamily:"Clash Grotesk", textAlign:"center", color:"rgba(100 100 100/ 70%)"}}>Dont have an account? <Link to="/signup" style={{color:"var(--color)", fontWeight:600}}>Sign-in</Link></Typography>
                         
                         </div>
                         
