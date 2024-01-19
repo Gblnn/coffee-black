a533a1694bd2285d199e9d45781b87ab1c8e8282
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-19 15:20:09 +0400

    Update post.tsx

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -90,7 +90,7 @@ export default function Post(props:Props) {
                     <div style={{display:"flex", margin:"0.5rem"}}>
 
                     
-                    <p style={{fontSize:"0.85rem"}}><strong>{props.author}</strong>{showMore? props.bio : props.bio.substring(0, 75)}
+                    <p style={{fontSize:"0.85rem", lineHeight:"1.25rem"}}><strong>{props.author}</strong>{showMore? props.bio : props.bio.substring(0, 75)}
                     {
                     props.bio.length>75?<a className='more' style={{fontSize:"0.8rem",fontWeight:500, marginLeft:"0.35rem",paddingLeft:"0.5rem",paddingRight:"0.5rem", cursor:"pointer"}} onClick={()=>setShowMore(!showMore)}>{showMore?"Less":"More"}</a>:null
                     }
