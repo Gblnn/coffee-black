d3344bcef471b7ba015173e7f24dc875d484336c
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-19 11:37:29 +0400

    Update commentbutton.tsx

--- src/components/buttons/commentbutton.tsx
+++ src/components/buttons/commentbutton.tsx
@@ -90,7 +90,7 @@ export default function CommentButton(props:Props){
                     <DrawerTitle style={{textAlign:"center"}}>Comments</DrawerTitle>
                     <DrawerDescription></DrawerDescription>
                 </DrawerHeader>
-                <div style={{height:"65svh", border:"", width:"100%", display:"flex", flexFlow:"column",justifyContent:"flex-end", padding:"1rem", gap:"1rem"}}>
+                <div style={{height:"65svh", border:"", width:"100%", display:"flex", flexFlow:"column",justifyContent:"flex-end", padding:"1rem", gap:"0rem"}}>
                     
                         {
                     
