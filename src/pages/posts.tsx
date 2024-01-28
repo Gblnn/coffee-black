2385a0e523d85a1e369243064a4b4d94d40237cc
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-28 23:01:25 +0400

    Update posts.tsx

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,4 +1,5 @@
 import ChatProfile from "@/components/chat-profile"
+import { Edit } from "lucide-react"
 
 export default function Posts(){
     return(
@@ -8,7 +9,14 @@ export default function Posts(){
             <div style={{width:"100%", height:"100%", padding:"", border:"", borderRadius:"1rem", background:"var(--semi)", display:"flex"}}>
                 <div className="inbox-chats" style={{border:""}}>
                     <div style={{border:"", width:"100%", height:"100%", display:"flex", flexFlow:"column"}}>
+                        <div style={{width:"100%", paddingBottom:"1rem", boxShadow:"1px 1px 50px var(--card-color)", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
                         <h1 style={{fontWeight:"600", fontSize:"1.5rem"}}>Chats</h1>
+                        <button className="hoverable" style={{padding:"0.5rem"}}>
+                        <Edit width="1.25rem" color="salmon"/>
+                        </button>
+                        
+                        </div>
+                        
                         <div className="chats" style={{overflowY:"auto", width:"100%", height:"100%", border:"", paddingTop:"1rem"}}>
                             <ChatProfile profile="/coffee.png" user="gbln___" message="Preview" notify={true}/>
                             <ChatProfile profile="/coffee-bag-color.png" user="moozkeen" message="Preview"/>
